0cfe51e5ad642e203613f01100cd27e2e6c68a82
Marina Shakarova <shakarova@profit-lab.ru>
2022-02-24 00:41:28 +0700

    домашнее задание #1

---
+++ functionsForNumbers.js
@@ -0,0 +1,47 @@
+const ERROR_MESSAGE = 'Некорректный ввод!';
+
+const getNumber = (order) => {
+	let number = prompt(`Введите ${order} число: `);
+	return number && Number(number);
+}
+
+const printError = () => {
+	console.log(ERROR_MESSAGE);
+	return;
+}
+
+const isValid = (number) => {
+	if (isNaN(number)) {
+		return printError();
+	}
+	return true;
+}
+
+const numberToBase = () => {
+	const firstNumber = getNumber('первое');
+	const secondNumber = getNumber('второе');
+	const IS_VALID = !isNaN(firstNumber) && !isNaN(secondNumber) && secondNumber >= 2 && secondNumber <= 10;
+
+	if (!IS_VALID) {
+		return printError();
+	}
+
+	console.log(firstNumber.toString(secondNumber));
+}
+
+const addAndDivide = () => {
+	let firstNumber = getNumber('первое');
+	if (!isValid(firstNumber)) {
+		return;
+	}
+
+	let secondNumber = getNumber('второе');
+	if (!isValid(secondNumber)) {
+		return;
+	}
+	
+	console.log(`Ответ: ${firstNumber + secondNumber}, ${firstNumber / secondNumber}.`);
+}
+
+document.querySelector('#base').addEventListener('click', numberToBase);
+document.querySelector('#operations').addEventListener('click', addAndDivide);
