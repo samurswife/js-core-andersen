6fafddf23fe277f0c4a70f7aa186dc23439eb2ff
Marina Shakarova <shakarova@profit-lab.ru>
2022-03-15 15:37:23 +0700

    Домашняя работа №3

---
+++ functionsForArrays.js
@@ -0,0 +1,56 @@
+function myFilter(callback, context) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+    const index = i;
+    const array = this;
+
+    if (callback.call(context, element, index, array)) {
+      result.push(element);
+    }
+  }
+
+  return result;
+}
+
+function myMap(callback, context) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+    const index = i;
+    const array = this;
+
+    const newElement = callback.call(context, element, index, array);
+
+    result.push(newElement);
+  }
+
+  return result;
+}
+
+function myForEach(callback, context) {
+  for (let i = 0; i < this.length; i++) {
+    callback.call(context, this[i], i, this);
+  }
+}
+
+function myReduce(callback, initialValue) {
+  let accumulator = initialValue;
+
+  for (let i = 0; i < this.length; i++) {
+    if (accumulator !== undefined) {
+      accumulator = callback(accumulator, this[i], i, this);
+    } else {
+      accumulator = this[i];
+    }
+  }
+
+  return accumulator;
+}
+
+Array.prototype.myFilter = myFilter;
+Array.prototype.myMap = myMap;
+Array.prototype.myForEach = myForEach;
+Array.prototype.myReduce = myReduce;
